cd5470696ecf7efeafd6970c15c3b0eb0c7fa0b5
Tolga Taş <tolga.tas@marti.tech>
2022-04-08 20:27:47 +0300

    First Commit
    Created game,
    Added Tile Background for creating Board,
    Added Tiles to randomly distribute,
    Added scripts for Board and Background.

---
+++ Row Match/Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    public GameObject[] tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialize()
+    {
+        int tileToUse = Random.Range(0, tiles.Length);
+        GameObject tile = Instantiate(tiles[tileToUse], transform.position, Quaternion.identity);
+        tile.transform.parent = this.transform;
+        tile.name = "Tile " + this.gameObject.name;
+    }
+}
---
+++ Row Match/Assets/Scripts/Board.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] gameBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameBoard = new BackgroundTile[width, height];
+        Configure();
+    }
+
+    private void Configure()
+    {
+        for (int i = 0; i < width; i ++)
+        {
+            for (int j = 0; j < height; j ++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "( " + i + ", " + j + " )";
+            }
+        }
+    }
+}
