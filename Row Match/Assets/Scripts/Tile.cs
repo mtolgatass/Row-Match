edb0af0837742fc27f396d9f6808bf365810793a
Tolga Taş <tolga.tas@marti.tech>
2022-04-09 17:38:29 +0300

    Refactor
    Prevented drawing ui at each time update function called.

--- Row Match/Assets/Scripts/Tile.cs
+++ Row Match/Assets/Scripts/Tile.cs
@@ -4,58 +4,30 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-
-    public int initialColumn;
-    public int initialRow;
-    public int destinationColumn;
-    public int destinationRow;
+    // MARK: - Private Variables
+    private int column;
+    private int row;
     private GameObject destinationTile;
     private Board board;
     private Vector2 firstTouchCoordinates;
     private Vector2 finalTouchCoordinates;
     private Vector2 tempPosition;
-    public float swipeAngle = 0;
-    // Start is called before the first frame update
+    private float swipeAngle = 0;
+
     void Start()
     {
         board = FindObjectOfType<Board>();
-        destinationColumn = (int)transform.position.x;
-        destinationRow = (int)transform.position.y;
+        column = (int)transform.position.x;
+        row = (int)transform.position.y;
 
-        initialRow = destinationRow;
-        initialColumn = destinationColumn;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        destinationColumn = initialColumn;
-        destinationRow = initialRow;
-        if(Mathf.Abs(destinationColumn - transform.position.x) > .1)
-        {
-            // Move Towards
-            tempPosition = new Vector2(destinationColumn, transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
-        } else
-        {
-            tempPosition = new Vector2(destinationColumn, transform.position.y);
-            transform.position = tempPosition;
-            board.allTiles[initialColumn, initialRow] = this.gameObject;
-        }
-
-        if (Mathf.Abs(destinationRow - transform.position.y) > .1)
-        {
-            // Move Towards
-            tempPosition = new Vector2(transform.position.x, destinationRow);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
-        }
-        else
-        {
-            tempPosition = new Vector2(transform.position.x, destinationRow);
-            transform.position = tempPosition;
-            board.allTiles[initialColumn, initialRow] = this.gameObject;
-        }
+        performChanges();
     }
+
+    // MARK: - Private Functions
 
     private void OnMouseDown()
     {
@@ -68,38 +40,106 @@ public class Tile : MonoBehaviour
         CalculateAngle();
     }
 
-    void CalculateAngle()
+    private void CalculateAngle()
     {
         swipeAngle = Mathf.Atan2(finalTouchCoordinates.y - firstTouchCoordinates.y, finalTouchCoordinates.x - firstTouchCoordinates.x) * 180 / Mathf.PI;
         MoveTile();
     }
 
-    void MoveTile()
+    private void MoveTile()
     {
-        if (swipeAngle > -45 && swipeAngle <= 45 && initialColumn < board.width)
+        if (swipeAngle > -45 && swipeAngle <= 45 && column + 1 < board.width)
         {
             // Swipe Right
-            destinationTile = board.allTiles[initialColumn + 1, initialRow];
-            destinationTile.GetComponent<Tile>().initialColumn -= 1;
-            initialColumn += 1;
-        } else if (swipeAngle > 45 && swipeAngle <= 135 && initialRow < board.height)
+            SwipeRightAction();
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row + 1 < board.height)
         {
             // Swipe Up
-            destinationTile = board.allTiles[initialColumn, initialRow + 1];
-            destinationTile.GetComponent<Tile>().initialRow -= 1;
-            initialRow += 1;
-        } else if ((swipeAngle > 135 || swipeAngle <= -135) && initialColumn > 0)
+            SwipeUpAction();
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0)
         {
             // Swipe Left
-            destinationTile = board.allTiles[initialColumn - 1, initialRow];
-            destinationTile.GetComponent<Tile>().initialColumn += 1;
-            initialColumn -= 1;
-        } else if (swipeAngle < -45 && swipeAngle >= -135 && initialRow > 0)
-        {
-            // Swipe Dow
-            destinationTile = board.allTiles[initialColumn, initialRow - 1];
-            destinationTile.GetComponent<Tile>().initialRow += 1;
-            initialRow -= 1;
+            SwipeLeftAction();
         }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0)
+        {
+            // Swipe Down
+            SwipeDownAction();
+        }
+    }
+
+    // MARK: - Private Swipe Functions
+    private void SwipeRightAction()
+    {
+        destinationTile = board.allTiles[column + 1, row];
+        destinationTile.GetComponent<Tile>().column -= 1;
+        column += 1;
+    }
+
+    private void SwipeLeftAction()
+    {
+        destinationTile = board.allTiles[column - 1, row];
+        destinationTile.GetComponent<Tile>().column += 1;
+        column -= 1;
+    }
+
+    private void SwipeUpAction()
+    {
+        destinationTile = board.allTiles[column, row + 1];
+        destinationTile.GetComponent<Tile>().row -= 1;
+        row += 1;
+    }
+
+    private void SwipeDownAction()
+    {
+        destinationTile = board.allTiles[column, row - 1];
+        destinationTile.GetComponent<Tile>().row += 1;
+        row -= 1;
+    }
+
+    // MARK: - Private Perform Change Functions
+
+    private void checkAndPerformHorizontalChanges()
+    {
+        if (Mathf.Abs(column - transform.position.x) > .1)
+        {
+            tempPosition = new Vector2(column, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .3f);
+        }
+        else
+        {
+            if (row != transform.position.y)
+            {
+                tempPosition = new Vector2(column, transform.position.y);
+                transform.position = tempPosition;
+                board.allTiles[column, row] = this.gameObject;
+            }
+        }
+    }
+
+    private void checkAndPerformVerticalChanges()
+    {
+        if (Mathf.Abs(row - transform.position.y) > .1)
+        {
+            tempPosition = new Vector2(transform.position.x, row);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .3f);
+        }
+        else
+        {
+            if (column != transform.position.x)
+            {
+                tempPosition = new Vector2(transform.position.x, row);
+                transform.position = tempPosition;
+                board.allTiles[column, row] = this.gameObject;
+            }
+        }
+    }
+
+    private void performChanges()
+    {
+        checkAndPerformHorizontalChanges();
+        checkAndPerformVerticalChanges();
     }
 }
